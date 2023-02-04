b2901e0f28bd9702a2f563bd7c4cfd517c3c5303
♕︎Deepesh-さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2023-02-04 18:10:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,16 @@ import { fileURLToPath } from 'url'
 
 
 //Make Changes According to Your Need
-global.bname = 'Wiley-md'     //Your Bot Name
-global.oname = 'Gariox & Shizo'     //Your Name
-global.iglink = 'https://Instagram.com/shizo_the_tech'     //Your Instagram Link
+global.bname = 'BayMax-md'     //Your Bot Name
+global.oname = 'Toshi_kun x Shizo x Gariox 3d'     //Your Name
+global.iglink = 'https://instagram.com/itz_toshi_02'     //Your Instagram Link
 global.ytlink = 'https://YouTube.com/@errormodsyt'      //Your YouTube Channel Link
-global.stkpack = 'Wiley-MD'      //Here name tha your want on sticker
+global.stkpack = 'Toshi-Kun'      //Here name tha your want on sticker
 
 
 
 global.owner = [
+  ['919609900020', 'Toshi_Kun', true],
   ['919172389527', 'Shizo The Techie', true],
   ['916207142994', 'Gariox 3D', true]
 ] 
@@ -20,15 +21,15 @@ global.prems = []
 
 
 // Sticker WM
-global.packname = 'TE DEVELOPERS'
-global.author = 'Wiley-md'
+global.packname = 'TE Developers'
+global.author = 'Toshi_kun'
 
 global.copyright = 'TE DEVELOPERS'
-global.packname = 'Wiley-md'
+global.packname = 'Toshi_kun'
 global.author = 'TE DEVELOPERS'
 
 //sticker WM
-global.stkpack = 'Wiley-md'
+global.stkpack = 'BayMax-md'
 global.stkowner = 'TE DEVELOPERS'
 
 let file = fileURLToPath(import.meta.url)
